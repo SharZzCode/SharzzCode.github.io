3e7adcfc38cbdfd69be4d816da63c1c2f16590ad
Sharz <rikigamer160@gmail.com>
2022-08-21 15:31:05 +0200

    modifiche al portfolio
    descrizione fazocca

--- script.js
+++ script.js
@@ -1,2 +1,26 @@
-import Typed from 'typed.js';
+/* SWITCH COLOR MODE */
+
+let switchModeBtn = document.querySelector(".switchModeBtn");
+let icon = document.querySelector(".fa-sun");
+
+
+switchModeBtn.addEventListener("click", () => {
+    icon.classList.toggle("fa-moon")
+    icon.classList.toggle("fa-sun")
+})
+
+
+/* DISPLAY PROJECT TITLE */
+
+let mediaElement = document.querySelector(".media-element");
+let projectTitle = document.querySelector(".project-title");
+
+mediaElement.addEventListener("mouseover", (event) => {  
+    projectTitle.style.display = "block";
+})
+
+mediaElement.addEventListener("mouseout", () => {
+    projectTitle.style.display = "none";
+})
+
 
